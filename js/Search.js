a18df596296a8b00e0da04e609129a1850c07fed
kelly <kelly.n.waite@gmail.com>
2017-01-29 20:53:38 -0700

    Create ShowCard component and map over json

--- js/Search.js
+++ js/Search.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import preload from '../public/data.json'
+import ShowCard from './ShowCard'
 
 const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show, i) => {
+        {preload.shows.map( show => {
           return (
-            <div className='show-card' key={show + i}>
-              <img src={`/public/img/posters/${show.poster}`} alt={`${show.title} poster`}/>
-              <h3>{show.title}</h3>
-              <h4>({show.year})</h4>
-              <p>{show.description}</p>
-            </div>
+            <ShowCard show={show} key={show.imdbID} />
           )
         })}
       </div>
